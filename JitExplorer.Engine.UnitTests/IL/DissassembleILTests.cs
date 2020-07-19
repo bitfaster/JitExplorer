3a9dc4c769f386ac848b4e7cba6d9f8f4ab11041
Alex Peck <amwpeck@gmail.com>
2020-07-18 18:22:21 -0700

    format IL

---
+++ JitExplorer.Engine.UnitTests/IL/DissassembleILTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace JitExplorer.Engine.UnitTests.IL
+{
+    public class DissassembleILTests
+    {
+        private readonly ITestOutputHelper output;
+
+        public DissassembleILTests(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Fact]
+        public void Test()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            var m = this.GetType().GetMethod(nameof(Test));
+
+            string data = JitExplorer.Engine.IL.Formatter.FormatMethodBody(m);
+
+            this.output.WriteLine(data);
+        }
+    }
+}
