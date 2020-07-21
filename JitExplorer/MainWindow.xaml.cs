99bbe59b8b4358a8cb8cd56b983d5ce15b749a1b
Alex Peck <amwpeck@gmail.com>
2020-07-20 20:41:09 -0700

    extract c# editor (#49)
    * extract c# editor
    
    * notes
    
    * rename

--- JitExplorer/MainWindow.xaml.cs
+++ JitExplorer/MainWindow.xaml.cs
@@ -1,11 +1,8 @@
-﻿using ICSharpCode.AvalonEdit.CodeCompletion;
-using ICSharpCode.AvalonEdit.Editing;
-using JitExplorer.Completion;
+﻿using ICSharpCode.AvalonEdit.Editing;
 using JitExplorer.Component;
 using JitExplorer.Engine.Compile;
 using JitExplorer.Model;
 using MahApps.Metro.Controls;
-using Microsoft.CodeAnalysis.Formatting;
 using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
@@ -25,8 +22,6 @@ namespace JitExplorer
     /// </summary>
     public partial class MainWindow : MetroWindow 
     {
-        private readonly RoslynCodeCompletion codeCompletion;
-
         public AppModel AppModel { get; set; }
 
         public MainWindow()
@@ -40,13 +35,6 @@ namespace JitExplorer
             this.AssemblerView.MouseDoubleClick += AssemblerView_MouseDoubleClick;
 
             this.Loaded += ThinBorder.SetThinBorder;
-
-            // in the constructor:
-            this.CodeEditor.TextArea.TextEntering += textEditor_TextArea_TextEntering;
-            this.CodeEditor.TextArea.TextEntered += textEditor_TextArea_TextEntered;
-
-            this.codeCompletion = new RoslynCodeCompletion(Compiler.MetadataReferences);
-            this.codeCompletion.Initialize();
 
             if (IntPtr.Size == 8)
             { 
@@ -140,43 +128,6 @@ namespace JitExplorer
         private void Exit(object sender, RoutedEventArgs e)
         {
             this.Close();
-        }
-
-        CompletionWindow completionWindow;
-
-        void textEditor_TextArea_TextEntered(object sender, TextCompositionEventArgs e)
-        {
-            if (e.Text == ".")
-            {
-                var result = codeCompletion.CompleteAsync(this.CodeEditor.Text, this.CodeEditor.CaretOffset, '.').Result;
-
-                // Open code completion after the user has pressed dot:
-                completionWindow = new CompletionWindow(this.CodeEditor.TextArea);
-                completionWindow.CompletionList.CompletionData.AddRange(result);
-
-                // TODO: how to correctly style this?
-                completionWindow.CompletionList.Background = SystemColors.ControlDarkDarkBrush;
-
-                completionWindow.Show();
-                completionWindow.Closed += delegate {
-                    completionWindow = null;
-                };
-            }
-        }
-
-        void textEditor_TextArea_TextEntering(object sender, TextCompositionEventArgs e)
-        {
-            if (e.Text.Length > 0 && completionWindow != null)
-            {
-                if (!char.IsLetterOrDigit(e.Text[0]))
-                {
-                    // Whenever a non-letter is typed while the completion window is open,
-                    // insert the currently selected element.
-                    completionWindow.CompletionList.RequestInsertion(e);
-                }
-            }
-            // Do not set e.Handled=true.
-            // We still want to insert the character that was typed.
         }
 
         private void ShowLineNumbersHandler(object sender, RoutedEventArgs e)
