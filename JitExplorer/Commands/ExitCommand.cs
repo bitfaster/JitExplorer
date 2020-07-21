08e1eee45fe58c3875e4e14226e6a98288560d96
Alex Peck <amwpeck@gmail.com>
2020-07-20 21:07:21 -0700

    exit cmd (#51)

---
+++ JitExplorer/Commands/ExitCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+
+namespace JitExplorer.Commands
+{
+    public class ExitCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
