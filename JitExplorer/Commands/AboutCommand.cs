1979f560c8473ce23058d4744499b85986a03cc5
Alex Peck <amwpeck@gmail.com>
2020-07-20 20:51:04 -0700

    extract about command

---
+++ JitExplorer/Commands/AboutCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows.Input;
+
+namespace JitExplorer.Commands
+{
+    public class AboutCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            string url = @"https://github.com/bitfaster/JitExplorer";
+
+            try
+            {
+                Process.Start(url);
+            }
+            catch
+            {
+                // hack because of this: https://github.com/dotnet/corefx/issues/10361
+                url = url.Replace("&", "^&");
+                Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+            }
+        }
+    }
+}
